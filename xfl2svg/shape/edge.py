224570df3534f6673f1960b14d12ea737d644f4f
PluieElectrique <41453973+PluieElectrique@users.noreply.github.com>
2022-07-03 00:44:31 -0700

    Explain why non-existant stroke IDs are ignored

--- xfl2svg/shape/edge.py
+++ xfl2svg/shape/edge.py
@@ -399,6 +399,7 @@ def xfl_edge_to_svg_path(
                 fill_edges.append((list(reversed(point_list)), fill_id_right))
 
             # Convert right away since we don't need to join anything into shapes
+            # Also, ignore stroke IDs that don't exist, as Animate seems to do.
             if stroke_id is not None and stroke_id in stroke_styles:
                 stroke_paths[stroke_id].append(point_list_to_path_format(point_list))
 
